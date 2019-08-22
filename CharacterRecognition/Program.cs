ac3627744bcf4d75b2c979dbae4cce29e81a7965
fantexi2019 <54256828+fantexi2019@users.noreply.github.com>
2019-08-22 09:12:39 +0800

    Update Program.cs

--- CharacterRecognition/Program.cs
+++ CharacterRecognition/Program.cs
@@ -17,6 +17,7 @@ namespace CharacterRecognition
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWnd());
+            //ggggggg
         }
     }
 }
